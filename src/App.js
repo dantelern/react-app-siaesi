a0e0276e5c1fc3f8133ca9db37c83f7db244b6c0
dantelern <110490913+dantelern@users.noreply.github.com>
2024-06-04 09:43:19 -0300

    Saque comentario boludo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/data') // URL de tu API
+    fetch('http://localhost:5000/api/data')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error:', error));
