fa2bb868c6be7d968b27537a0d4b784745b9c649
dantelern <110490913+dantelern@users.noreply.github.com>
2024-07-02 08:32:00 -0300

    Pude cambiar DataDisplay

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -2,20 +2,41 @@ import React, { useEffect, useState } from 'react';
 
 const DataDisplay = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('https://db-esi.vercel.app/') /// http://localhost:3001/api/data
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error agarrando datos:', error));
+    fetch('https://db-esi.vercel.app/') // http://localhost:3001/api/data
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error agarrando datos: {error.message}</p>;
+  }
 
   return (
     <div>
       <h1>Data de base de datos</h1>
       <ul>
         {data.map(item => (
-          <li key={item.id}>{item.nombre}</li> 
+          <li key={item.id}>{item.nombre}</li>
         ))}
       </ul>
     </div>
