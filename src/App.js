9fb6cb4155ea3b54b6fa1a5fbcc4796fb10023aa
dantelern <110490913+dantelern@users.noreply.github.com>
2024-07-03 09:22:26 -0300

    Agrego server.js (users), importo axios, etc

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {
   Link
 } from 'react-router-dom';
 import './App.css';
-import logo from './logo.svg';
 import DataDisplay from './DataDisplay.js';
+import axios from 'axios';
 
 function Home() {
   return (
---
+++ src/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+let users = [];
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/api/users', (req, res) => {
+  const user = { id: users.length + 1, ...req.body };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+app.put('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (user) {
+    Object.assign(user, req.body);
+    res.json(user);
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  const index = users.findIndex(u => u.id === parseInt(req.params.id));
+  if (index !== -1) {
+    users.splice(index, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Usuario no encontrado');
+  }
+});
+
+module.exports = app;
